3f8935b54067051140f93f51803c916b603cfc55
Gariganti Bhuvaneswari <garigantibhuvaneswari@gmail.com>
2026-02-16 12:06:27 +0530

    🚀 Day 14 - index.html - 12:06:23 pm

--- Day-14/script.js
+++ Day-14/script.js
@@ -1,7 +1,38 @@
-// Day 14 Logic Practice
-console.log('Lab Session 14 Started');
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
+let maxAttempts = 10;
 
-const display = document.getElementById('display');
-display.innerText = 'Logic Engine Online';
+function checkGuess() {
+  let userGuess = Number(document.getElementById("guessInput").value);
+  let message = document.getElementById("message");
+  let attemptsText = document.getElementById("attempts");
 
-// Practice your JS code here...
+  attempts++;
+
+  while (attempts <= maxAttempts) {
+
+    if (userGuess === randomNumber) {
+      message.innerHTML = "🎉 Correct! You guessed the number!";
+      message.style.color = "green";
+      break;
+    } 
+    else if (userGuess > randomNumber) {
+      message.innerHTML = "📉 Too High!";
+      message.style.color = "red";
+      break;
+    } 
+    else {
+      message.innerHTML = "📈 Too Low!";
+      message.style.color = "blue";
+      break;
+    }
+  }
+
+  attemptsText.innerHTML = `Attempts: ${attempts} / ${maxAttempts}`;
+
+  if (attempts === maxAttempts && userGuess !== randomNumber) {
+    message.innerHTML = "❌ Game Over! Number was " + randomNumber;
+    message.style.color = "darkred";
+    document.querySelector("button").disabled = true;
+  }
+}
